c63a8f08e47a655b2b077e1632d1e66ee7add72d
zainalidev <zain4659@gmail.com>
2019-11-19 22:54:02 +0100

    added event emitter  and tests

--- js/bit.js
+++ js/bit.js
@@ -1,19 +1,51 @@
-class Bit {
+/* 
+EventEmitter based on: 
+https://netbasal.com/javascript-the-magic-behind-event-emitter-cce3abcbcef9
+*/
+class EventEmitter {
     constructor() {
+        this.events = {};
+    }
+
+    emit(eventName, data) {
+        const event = this.events[eventName];
+        if (event) {
+            event.forEach(fn => {
+                fn.call(null, data);
+            });
+        }
+    }
+
+    subscribe(eventName, fn) {
+        if (!this.events[eventName]) {
+            this.events[eventName] = [];
+        }
+        this.events[eventName].push(fn);
+        return () => {
+            this.events[eventName] = this.events[eventName].filter(eventFn => fn !== eventFn);
+        }
+    }
+}
+/* Bit created by Zain Ali */
+class Bit extends EventEmitter {
+    constructor() {
+        super();
         this.version = "0.1";
         this.author = "Zain Ali";
         this.slogan = "Hobby project :D";
     }
-    check(){
+    check() {
         let c = document.querySelectorAll('.bit');
         let i, l;
         //dom list
         var d = [];
-        if(c.length == 0) {
+        this.emit('hello', c);
+        this.emit('hello_private', c);
+        if (c.length == 0) {
             throw new Error("Can't find the containers for Bit");
-        } 
+        }
         for (i = 0; i < c.length; i++) {
-            c[i].innerHTML = 'Bit running on...'+document.domain;
+            c[i].innerHTML = 'Bit running on...' + document.domain;
             c[i].Bit = this.version;
             d.push(c[i]);
         }
