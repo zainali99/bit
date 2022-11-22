6ee2d692d5b7028717730c91d849f2d883064bc0
zain <zain4659@gmail.com>
2022-11-22 12:54:49 +0100

    checkpoint drag/drop work in progress

--- js/bit.js
+++ js/bit.js
@@ -29,21 +29,39 @@ class EventEmitter {
 
 /* Bit created by Zain Ali */
 class Bit extends EventEmitter {
-    constructor(element) {
+    constructor(element, options={}) {
         super();
         this.id = `bit-${Math.random().toString(16).substring(2)}`;
         this.log(element)
         this.version = "0.1";
         this.author = "Zain Ali";
-        this.slogan = "Hobby project :D";
+        this.slogan = "";
         this.files = [];
         this.element = element;
-        this.max_args_num = 5;
+        this.max_args_num = 5; // will deprecated
         this.mode = 'dev';
         this.max_scan = 1;
-        this.init();
+        this.language = options.language || 'en';
         // plugins system
         this.plugins = [];
+        this.DOM_IDS = {
+            'HIDDEN_INPUT': '#@bit__hidden__input',
+        };
+        this.i18n_dict = {
+            'en': {
+                DRAG_AND_DROP_TEXT: 'Drag/Drop your files for uploading...',
+            },
+            'it': {
+                DRAG_AND_DROP_TEXT: '',
+            }
+        }
+
+        this.i18n = this.get_i18n_dict()  
+
+
+        
+
+        this.init();
 
     }
     log() {
@@ -64,29 +82,56 @@ class Bit extends EventEmitter {
         }
         fr.readAsDataURL(data[0]);
     }
+    applyBasicCSS() {
+        this.element.insertAdjacentHTML('beforeend', `
+        <style>
+        #${this.id} {border:2px dashed;padding:10px;margin:10px}
+        </style>
+        `);
+    }
+    get_i18n_dict() {
+        // TODO add check key maybe in init.
+        return this.i18n_dict[this.language];
+    }
     init() {
-        // hello !
-        this.on('hello', data => {
-            console.log('Hello World !')
-        });
-        this.emit('hello');
-
         this.on('onChange', data => {
             this.log(`some change on instance: ${this.id}`,data)
         });
-        
+        this.applyBasicCSS();
+        this.element.setAttribute('id', this.id);
+        this.log(this.i18n)
+        this.element.insertAdjacentHTML('beforeend', `<div>${this.i18n.DRAG_AND_DROP_TEXT}</div>`);
+                
+
         // TODO: create a hidden input 
-        // TODO: a
         this.element.addEventListener('change', () => {
             let data = {}
             data.files = this.element.files;
             data.instance_id = this.id;
-            
-            
             this.emit('onChange',data)
         });
 
     }
+    onFileSelect(){
+
+    }
+    setupHiddenInput(){
+
+        const elem = this.$refs.muWrapper.querySelector(this.DOM_IDS.HIDDEN_INPUT)
+        if (elem) {
+          elem.remove()
+        }
+        let i = document.createElement('input');
+        i.setAttribute("type", "file");
+        i.setAttribute("multiple", "multiple");
+        i.setAttribute('accept', 'image/*')
+        i.setAttribute('style', 'visibility: hidden; position: absolute; top: 0px; left: 0px; height: 0px; width: 0px;')
+        i.setAttribute('id', this.DOM_IDS.HIDDEN_INPUT);
+        i.onchange = (e) => {
+          this.onFileSelect(e);
+        }
+        
+      }
 
 
 
