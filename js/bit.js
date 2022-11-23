513545c3ccb232063d165e20ea6329181aee936a
zain <zain4659@gmail.com>
2022-11-23 15:32:47 +0100

    added update func to add custom data on each file, added getFiles hook

--- js/bit.js
+++ js/bit.js
@@ -15,7 +15,8 @@ class EventEmitter {
             });
             return;
         }
-        throw new ReferenceError('[BIT-0000]: The Event you called:' + eventName + ' doesn\'t exist anymore, register it with: bit.on()');
+       
+        this.log('[BIT-0000]: The Event you called:' + eventName + ' doesn\'t exist anymore, register it with: bit.on()');
     }
     //register the events
     on(eventName, fn) {
@@ -31,7 +32,7 @@ class EventEmitter {
 class Bit extends EventEmitter {
     constructor(element, options={}) {
         super();
-        this.id = `bit-${Math.random().toString(16).substring(2)}`;
+        this.id = `bit-${this.get_random_key()}`;
         this.log(element)
         this.version = "0.1";
         this.slogan = "";
@@ -49,9 +50,12 @@ class Bit extends EventEmitter {
         this.i18n_dict = {
             'en': {
                 DRAG_AND_DROP_TEXT: 'Drag/Drop your files for uploading...',
+                DELETE_FILE: 'delete'
             },
             'it': {
                 DRAG_AND_DROP_TEXT: '',
+                DELETE_FILE: 'cancella',
+
             }
         }
 
@@ -60,24 +64,65 @@ class Bit extends EventEmitter {
         this.init();
 
     }
+    get_random_key() {
+        return Math.random().toString(16).substring(2)
+    }
     log() {
         if (this.mode !== 'dev') return;
         for (let i=0;i < arguments.length && arguments.length <= this.max_args_num; i++) {
             console.log('[BIT.JS]:',arguments[i])
         }
     }
+    createBitItem(file){
+        let e = document.createElement('div');
+        // check collission
+        file.token = `${this.get_random_key()}-${this.get_random_key()}`;
+        e.file = file;
+        // preload using new Image maybe...
+        e.className = "bit-core-item";
+        e.innerHTML = `<div class="bit-preview-wrapper" style="background-image:url('${file.src}')">
+        <button type="button" class="bit-delete-item-btn" data-token="${file.token}">${this.i18n.DELETE_FILE}</button>
+        </div>`
+        let t=this;
+        e.onclick = function(ev) {
+            ev.stopPropagation()
+            t.emit('bit_item_click', {
+                'element': e
+            })
+        };
+        e.querySelector('.bit-delete-item-btn').onclick = function(ev) {
+            ev.stopPropagation();
+            console.log(ev.target.parentNode)
+            ev.target.parentNode.remove()
+        }
+        this.element.appendChild(e)
+
+    }
     show_images() {
         console.log(this.files)
-        let h = "";
         this.files.forEach(i => {
-            h +=`<img width="100" src="${i.src}" />`;
+            if (i.already_parsed) return;
+            this.createBitItem(i)
+            i.already_parsed = true;
         });
-        this.element.innerHTML = h;
     }
     applyBasicCSS() {
         this.element.insertAdjacentHTML('afterend', `
         <style>
         #${this.id} {border:2px dashed;padding:10px;margin:10px}
+        #${this.id} .bit-core-item .bit-preview-wrapper {width: 100%;
+            height: 60px;cursor:pointer;
+            background-size: contain;
+            background-repeat: no-repeat;
+            background-position: center center;
+        }
+
+        #${this.id} .bit-core-item {min-width: 120px;
+            display: inline-block;
+            margin: 10px;
+            border: 1px solid;
+
+        }
         </style>
         `);
     }
@@ -86,8 +131,10 @@ class Bit extends EventEmitter {
         return this.i18n_dict[this.language];
     }
     init() {
+        
         this.on('onChange', data => {
             this.log(`some change on instance: ${this.id}`,data)
+            this.log(...this.getFiles(1))
         });
         this.applyBasicCSS();
         this.element.setAttribute('id', this.id);
@@ -103,24 +150,44 @@ class Bit extends EventEmitter {
         console.log(e.target.files)
         this.files = this.files.concat(...e.target.files)
         this.files.concat(e.target.files);
-        let promises = [];
-        for (var i=0; i<this.files.length;i++) {
-            let file = this.files[i];
-            promises.push(
-                new Promise((resolve) => {
-                    let fr = new FileReader();
-                    fr.onload = function() {
-                        file.src = fr.result;
-                        resolve();
-                    };
-                    fr.readAsDataURL(file);
-                })
-            )
-        }
-        await Promise.all(promises).then(() => {
-            this.show_images()
+        this.buildPreviews()
 
-        });
+    }
+
+    buildPreviews = () => {
+        console.log('called ')
+        let promises = [];
+
+        const generateThumbs = async () => {
+            console.log(this.files)
+            for (var i=0; i<this.files.length;i++) {
+                let file = this.files[i];
+                console.log(file, typeof file)
+                promises.push(
+                    new Promise((resolve) => {
+                        let fr = new FileReader();
+                        fr.onload = async function() {
+                            var res = await fetch(fr.result)
+                            res = await res.blob()
+                            console.log(res)
+                            file.src = URL.createObjectURL(res)
+                            resolve()
+                            
+                        };
+                        fr.readAsDataURL(file);
+                    })
+                )
+            }
+        };
+        // TODO: improve this part ?!
+        let timer =  setTimeout(async() => {
+            generateThumbs();
+            await Promise.all(promises).then(() =>{
+                this.show_images()
+                clearTimeout(timer);
+                this.emit('onChange')
+            });
+        },50);
     }
     onDragOver(ev) {
         ev.preventDefault();
@@ -148,7 +215,7 @@ class Bit extends EventEmitter {
             console.log(`… file[${i}].name = ${file.name}`);
           });
         }
-        this.bit.show_images(this, this.bit.files)
+        this.bit.buildPreviews()
     }
     async getThumbnail(file) {
         // const vm = this;
@@ -179,13 +246,49 @@ class Bit extends EventEmitter {
           this.onFileSelect(e);
         }
         document.body.appendChild(i);
+
         this.element.addEventListener('click', () => {
             i.click()
         })
         
-
-
-      }
+    }
+    update(token="", index=null,obj={}) {
+        // not recommended to update with index.
+        // TODO: improve performance
+        let update_count = 0;
+        this.files = this.files.map((f, i) => {
+            console.log(f, i)
+            if ((token && f.token == token) || typeof index == "number" && i==index) {
+                if (!f.additional_data) f.additional_data = {};
+                f.additional_data = Object.assign(f.additional_data, obj)
+                update_count +=1
+                return f;
+            }
+        });
+        return update_count;
+    }
+    getFiles(form_data=false){
+        if (!form_data) {
+            return this.files;
+        }
+        var fd = new FormData();
+        // format key-value
+        // each key contain a file information
+        let image_data = []
+        // let ins = this;
+        console.log(this.files)
+        for (var i=0; i<this.files.length;i++) {
+            if (!(this.files[i] instanceof File)) this.log('no file skipping'); continue;
+            image_data.push({
+                key: this.files[i].token,
+                ...this.files[i].additional_data 
+            })
+            fd.append(this.files[i].token,this.files[i])
+        }
+        fd.append('image_data', JSON.stringify(image_data))
+        return fd
+    }
+      
 
 
 
