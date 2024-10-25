3856d4b0c2634c7dbf5c110ede1e259205c9532d
zain ali <zain4659@gmail.com>
2024-10-25 16:20:03 +0200

    regular patch

--- js/bit.js
+++ js/bit.js
@@ -492,10 +492,10 @@ class Bit extends EventEmitter {
                 blob.name = `name${t.get_random_key()}.${extension}`;
                 const f = new File([blob], blob.name, {type:type});
                 delete data[i]['src'];
+                f.uploaded = data.uploaded || false; // <-- Add this line
                 delete data[i]['uploaded']
                 f.additional_data = {...data[i]}
                 // Set uploaded to true for manually created files
-                f.uploaded = data.uploaded || false; // <-- Add this line
                 t.files.push(f);
             });
         }
